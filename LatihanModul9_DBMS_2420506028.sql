992ef890bec0b85f9cf7907661e023da947889a9
Assep Wahid <wahidassep5@gmail.com>
2025-06-14 23:33:33 +0700

    Add files via upload

---
+++ LatihanModul9_DBMS_2420506028.sql
@@ -0,0 +1,190 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 14, 2025 at 06:31 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `universitas`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `view_mhs_simple` ()   BEGIN
+    SELECT NPM, nama, no_hp
+    FROM mahasiswa;
+END$$
+
+--
+-- Functions
+--
+CREATE DEFINER=`root`@`localhost` FUNCTION `tambah` (`angka1` INT, `angka2` INT) RETURNS INT(11)  BEGIN
+    RETURN angka1 + angka2;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_brg` char(3) NOT NULL,
+  `nama_brg` varchar(50) DEFAULT NULL,
+  `stok` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id_brg`, `nama_brg`, `stok`) VALUES
+('A10', 'Mouse', 17),
+('A11', 'Keyboard', 20),
+('A12', 'DVD R-W', 50),
+('A13', 'Modem', 7),
+('A14', 'Flashdisk', 25),
+('A15', 'Webcam', 11),
+('A16', 'Printer', 19),
+('A17', 'Scanner', 10);
+
+--
+-- Triggers `barang`
+--
+DELIMITER $$
+CREATE TRIGGER `deletechild` AFTER DELETE ON `barang` FOR EACH ROW delete from pembelian
+where id_brg = old.id_brg
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `inkremenStok` BEFORE INSERT ON `barang` FOR EACH ROW set new.stok = new.stok +1
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `NPM` char(5) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `alamat` varchar(50) DEFAULT NULL,
+  `id_kelurahan` int(11) DEFAULT NULL,
+  `jenis_kelamin` enum('L','P') DEFAULT NULL,
+  `no_hp` varchar(20) DEFAULT NULL,
+  `kode_prodi` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`NPM`, `nama`, `alamat`, `id_kelurahan`, `jenis_kelamin`, `no_hp`, `kode_prodi`) VALUES
+('12345', 'Imam Adi Nata', 'Kajen', 1, 'L', '081215529989', 1),
+('12346', 'Budi Sugandhi', 'Bekasi', 109, 'L', '085643433321', 2),
+('12347', 'Toha Sitohang', 'Medan', 103, 'L', '08998787876', 2),
+('12348', 'Megawati', 'Condong Catur', 107, 'P', '0839303058', 1),
+('12349', 'PRABOWO SUBIYANTO', 'JKT', 107, 'L', '08132809811', 0),
+('12780', 'Anis Baswedan', 'Jakarta', 234, 'L', '09898877663', NULL),
+('12786', 'GANJAR PRANOWO', 'JAWA TENGAH', 123, 'L', '087656356622', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembelian`
+--
+
+CREATE TABLE `pembelian` (
+  `id_pem` int(11) NOT NULL,
+  `id_brg` char(3) DEFAULT NULL,
+  `jml_beli` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pembelian`
+--
+
+INSERT INTO `pembelian` (`id_pem`, `id_brg`, `jml_beli`) VALUES
+(1, 'A10', 5),
+(2, 'A11', 3),
+(3, 'A12', 40),
+(4, 'A13', 1),
+(5, 'A14', 4),
+(6, 'A15', 2),
+(7, 'A16', 6),
+(8, 'A17', 3),
+(9, 'A10', 1),
+(10, 'A11', 2);
+
+--
+-- Triggers `pembelian`
+--
+DELIMITER $$
+CREATE TRIGGER `updatestok` AFTER INSERT ON `pembelian` FOR EACH ROW update barang
+set stok = stok + new.jml_beli
+where id_brg = new.id_brg
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `updatestokedit` AFTER UPDATE ON `pembelian` FOR EACH ROW update barang
+set stok = stok + (new.jml_beli - old.jml_beli)
+where id_brg = new.id_brg
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_brg`);
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`NPM`);
+
+--
+-- Indexes for table `pembelian`
+--
+ALTER TABLE `pembelian`
+  ADD PRIMARY KEY (`id_pem`),
+  ADD KEY `pembelian_ibfk_1` (`id_brg`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pembelian`
+--
+ALTER TABLE `pembelian`
+  ADD CONSTRAINT `pembelian_ibfk_1` FOREIGN KEY (`id_brg`) REFERENCES `barang` (`id_brg`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
